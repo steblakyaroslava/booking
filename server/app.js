1f7ed3d92a8b59eb4683ee0a5b37c91a4c2984cc
steblakyaroslava <steblak.yaroslava@sstudent.uzhnu.edu.ua>
2024-09-21 01:33:08 +0300

    setup env

--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+import { config } from "nodemon";
 
 const PORT = process.env.PORT || 5000;
 
@@ -7,6 +9,12 @@ app.get('/', (req, res)=>{
     res.send('Hello World!')
 })
 
+export const db_url = config.get<String>("DB_TYPE") ? "local" 
+`mongodb:${config.get<string>("DB_USERNAME")}//:${config.get<string>("DB_PASSWORD")}@${config.get<string>("DB_HOST")}:${config.get<string>("DB_PORT")}/${config.get<string>("DB_NAME")}` : 
+`mongodb+srv:${config.get<string>("DB_USERNAME")}//:${config.get<string>("DB_PASSWORD")}@${config.get<string>("DB_CLUSTER")}.${config.get<string>("DB_HASH")}.mongodb.net/?retryWrites=true&w=majority&appName=${config.get<string>("DB_NAME")}`
+
+
 app.listen(PORT, ()=>{
+    mongoose.connect(db_url).then(res=>console.log('Connected to db'))
     console.log(`Server running: http://localhost:${PORT}`);
 })
